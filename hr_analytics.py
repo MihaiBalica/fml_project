03b5c1c1c038b86ebd56870146a7f135ed9309c6
Mihai Balica <mihai.balica@softvision.ro>
2021-02-12 21:08:19 +0200

    Re-arrange the code

--- hr_analytics.py
+++ hr_analytics.py
@@ -47,6 +47,11 @@ def pie_plot(data, col, title=None):
     plt.show()
 
 
+def pair_plot(data):
+    sns.pairplot(data)
+    plt.show()
+
+
 def visualize_data(train_dataset_local):
     # looks like people from developed cities are less likely to look for a job change
     histogram_plot(train_dataset_local, 'city_development_index', hue='target', title='City development index')
@@ -83,30 +88,110 @@ def visualize_data(train_dataset_local):
     # visualize for last new job
     bar_plot(train_dataset_local, 'last_new_job', hue='target')
 
+    # pair_plot
+    pair_plot(train_dataset_local)
+
 
 def encoding_features(x):
     rel_exp_idx, edu_idx, comp_size_idx = [list(x.columns).index(col) for col in
                                            ['relevent_experience', 'education_level', 'company_size']]
-    x.iloc[:, rel_exp_idx] = x.iloc[:, rel_exp_idx].map({'No relevent experience': -1,
+    x.iloc[:, rel_exp_idx] = x.iloc[:, rel_exp_idx].map({'No relevent experience': 0,
                           'Has relevent experience': 1}).astype(int)
-    x.iloc[:, edu_idx] = x.iloc[:, edu_idx].map({'Primary School': -1,
-                                                        'High School': 1,
-                                                        'Graduate': 2,
-                                                        'Masters': 3,
-                                                        'Phd': 4}).astype(int)
-    x.iloc[:, comp_size_idx] = x.iloc[:, comp_size_idx].map({'<10': -1,
-                                                             '10/49': 1,
-                                                             '50-99': 2,
-                                                             '100-500': 3,
-                                                             '500-999': 4,
-                                                             '1000-4999': 5,
-                                                             '5000-9999': 6,
-                                                             '10000+': 7}).astype(int)
-    x.loc[(x['experience'] == '>20'), 'experience'] = 25
-    x.loc[(x['experience'] == '<1'), 'experience'] = -1
-    x.loc[(x['last_new_job'] == 'never'), 'last_new_job'] = -1
+    x.iloc[:, edu_idx] = x.iloc[:, edu_idx].map({'Primary School': 1,
+                                                        'High School': 2,
+                                                        'Graduate': 3,
+                                                        'Masters': 4,
+                                                        'Phd': 5}).astype(int)
+    x.iloc[:, comp_size_idx] = x.iloc[:, comp_size_idx].map({'0': 1,
+                                                             '<10': 2,
+                                                             '10/49': 3,
+                                                             '50-99': 4,
+                                                             '100-500': 5,
+                                                             '500-999': 6,
+                                                             '1000-4999': 7,
+                                                             '5000-9999': 8,
+                                                             '10000+': 9}).astype(int)
+    x.loc[(x['experience'] == '>20'), 'experience'] = 21
+    x.loc[(x['experience'] == '<1'), 'experience'] = 0
+    x.loc[(x['last_new_job'] == 'never'), 'last_new_job'] = 0
     x.loc[(x['last_new_job'] == '>4'), 'last_new_job'] = 5
     return x
+
+def processing_data(data):
+    print("Train dataset")
+    print(data.info())
+    # print("Test dataset")
+    # print(test_dataset.info())
+
+    print("Describe the dataset")
+    print(data.describe(include='all'))
+
+    # it shows that enrolled_university, education_level, experience and last_new_job features
+    # have less than 3% missing values, so will drop them
+    print("% of NULL values")
+    print(data.isnull().mean() * 100)
+    print("Total number of NULL values")
+    # total number of empty entries
+    print(data.isnull().sum())
+    print()
+
+    print("Dropping NULLs for what is under 3% missing data")
+    data.dropna(subset=['enrolled_university', 'education_level', 'experience', 'last_new_job'], inplace=True)
+    print("% of NULL values after dropping below 3% ")
+    print(data.isnull().mean() * 100)
+
+    # the "enrollee_id" is a unique number assigned for each employee so it is not a feature
+    data.drop('enrollee_id', axis=1, inplace=True)
+    print("Without enrollee_id")
+    print(data.head())
+    print()
+    # city and city_development_index are having similar information, they are strictly linked together so dropping city
+    data.drop('city', axis=1, inplace=True)
+    print("Without city")
+    print(data.head())
+
+    X = data.drop('target', axis=1)
+    y = data['target']
+    # replacing missing values
+    # these are the supported strategies: allowed_strategies = ["mean", "median", "most_frequent", "constant"]
+    # mode_imputer = SimpleImputer(strategy="most_frequent")
+    # mode_imputer.fit(X)
+    # X_imputed = mode_imputer.transform(X)
+    # print(X_imputed)
+    # X_df = pd.DataFrame(X_imputed, columns=X.columns, index=X.index)
+    # the above code leads to 76.686% accuracy score.
+
+    # handling NULL in a different way
+    X["gender"].fillna(value="Female", inplace=True)
+    X["major_discipline"].fillna(value="unknown", inplace=True)
+    X["company_size"].fillna(value="0", inplace=True)
+    X["company_type"].fillna(value="unknown", inplace=True)
+    X_df = pd.DataFrame(X, columns=X.columns, index=X.index)
+
+    # Label encoding relevant_experience, education_level, company_size, experience and last_new_job features
+    encoder = FunctionTransformer(encoding_features)
+    X_encoded = encoder.fit_transform(X_df)
+    print(X_encoded)
+
+    X_encoded['experience'] = X_encoded['experience'].astype(int)
+    X_encoded['last_new_job'] = X_encoded['last_new_job'].astype(int)
+    X_encoded['city_development_index'] = X_encoded['city_development_index'].astype(float)
+    X_encoded['city_development_index'] = X_encoded['city_development_index'].apply(lambda x: x * 100)
+    X_encoded['training_hours'] = X_encoded['training_hours'].astype(int)
+    print(X_encoded)
+    print(X_encoded.info())
+
+    # encoding and feature scaling remaining stuff
+    num_attribs = ['city_development_index', 'education_level', 'experience', 'company_size', 'last_new_job',
+                   'training_hours']
+    cat_attribs = ['gender', 'enrolled_university', 'major_discipline', 'company_type']
+    preprocessing = ColumnTransformer([
+        ("num", StandardScaler(), num_attribs),
+        ("cat", OneHotEncoder(drop='first', sparse=False), cat_attribs)
+    ], remainder="passthrough")
+
+    hr_processed = preprocessing.fit_transform(X_encoded)
+    return hr_processed, y
 
 
 ###########################################
@@ -117,75 +202,22 @@ def encoding_features(x):
 
 
 train_dataset = pd.read_csv('aug_train.csv')
+# test_dataset = pd.read_csv('aug_test.csv')
 # visualize_data(train_dataset_local=train_dataset)
 
 # target: 0 – Not looking for job change, 1 – Looking for a job change
 
 ########################################
-#                                      #
 #       Cleaning up the data           #
-#                                      #
 ########################################
-
-print(train_dataset.info())
-print()
-print(train_dataset.describe(include='all'))
-print()
-# it shows that enrolled_university, education_level, experience and last_new_job features
-# have less than 3% missing values, so will drop them
-print(train_dataset.isnull().mean() * 100)
-print()
-train_dataset.dropna(subset=['enrolled_university', 'education_level', 'experience', 'last_new_job'], inplace=True)
-print(train_dataset.isnull().mean() * 100)
-print()
+hr_data_processed, y_processed = processing_data(train_dataset)
 
 
-# While looking at the data, I could see that the "enrollee_id" is a unique number assigned for each employee so
-# it is not a feature. Dropping it
-train_dataset.drop('enrollee_id', axis=1, inplace=True)
-print(train_dataset.head())
-# city and city_development_index are having similar information, they are strictly linked together so dropping city
-train_dataset.drop('city', axis=1, inplace=True)
-
-X = train_dataset.drop('target', axis=1)
-y = train_dataset['target']
-# replacing missing values
-# these are the supported strategies: allowed_strategies = ["mean", "median", "most_frequent", "constant"]
-mode_imputer = SimpleImputer(strategy="most_frequent")
-mode_imputer.fit(X)
-X_imputed = mode_imputer.transform(X)
-print(X_imputed)
-X_df = pd.DataFrame(X_imputed, columns=X.columns, index=X.index)
-
-# Label encoding relevant_experience, education_level, company_size, experience and last_new_job features
-encoder = FunctionTransformer(encoding_features)
-X_encoded = encoder.fit_transform(X_df)
-print(X_encoded)
-
-X_encoded['experience'] = X_encoded['experience'].astype(int)
-X_encoded['last_new_job'] = X_encoded['last_new_job'].astype(int)
-X_encoded['city_development_index'] = X_encoded['city_development_index'].astype(float)
-X_encoded['training_hours'] = X_encoded['training_hours'].astype(int)
-print(X_encoded)
-print(X_encoded.info())
-
-
-# encoding and feature scaling remaining stuff
-num_attribs = ['city_development_index', 'training_hours']
-cat_attribs = ['gender', 'enrolled_university', 'major_discipline', 'company_type']
-preprocessing = ColumnTransformer([
-    ("num", StandardScaler(), num_attribs),
-    ("cat", OneHotEncoder(drop='first', sparse=False), cat_attribs)
-], remainder="passthrough")
-
-hr_processed = preprocessing.fit_transform(X_encoded)
-print(hr_processed)
-
-X_train, X_test, y_train, y_test = train_test_split(hr_processed, y, test_size=0.2, shuffle=True, stratify=y)
-print(X_train)
-print(y_test)
-print(X_test)
-print(y_test)
+X_train, X_test, y_train, y_test = train_test_split(hr_data_processed, y_processed, test_size=0.2, shuffle=True, stratify=y_processed)
+# print(X_train)
+# print(y_train)
+# print(X_test)
+# print(y_test)
 
 
 classifier = SVC(kernel='rbf', random_state=0)
